8a932cb8646b45a3f4dc0878b2e1c2a57b3d14ac
Luis Henrique <senseyluiz@gmail.com>
2026-05-04 16:40:04 -0300

    MElhora na extração de dados da API

--- scripts/extract.py
+++ scripts/extract.py
@@ -1,26 +1,41 @@
 import requests
+import json
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 def getData():
-    URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey={os.getenv('APIKEY')}"
+    API_KEY = os.getenv("APIKEY")
+    SYMBOL = "IBM"
+
+    URL = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={SYMBOL}&apikey={API_KEY}"
     response = requests.get(URL)
     response.raise_for_status()
     data = response.json()
-    print(data)
+
+    if "Time Series (Daily)" not in data:
+        raise Exception(f"Erro na API: {data}")
+
     time_series = data.get("Time Series (Daily)", {})
-    print(time_series)
+
     actions = []
     for date, values in time_series.items():
         action_date = {
             "date": date,
-            "open": values.get("1. open"),
-            "high": values.get("2. high"),
-            "low": values.get("3. low"),
-            "close": values.get("4. close"),
-            "volume": values.get("5. volume")
+            "open": float(values.get("1. open")),
+            "high": float(values.get("2. high")),
+            "low": float(values.get("3. low")),
+            "close": float(values.get("4. close")),
+            "volume": int(values.get("5. volume"))
         }
         actions.append(action_date)
     return actions
+
+if __name__ == "__main__":
+    actions = getData()
+
+    with open("../database/actions.json", "w") as outfile:
+        json.dump(actions, outfile, indent=4)
+
+    print("\33[32m Dados extraídos e salvos com sucesso! \33[m")
 
